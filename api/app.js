b60052ed09ee85a767eecf1f413f19696bccf8d1
IvanM <mahankovage@gmail.com>
2022-08-18 22:50:52 +0300

    arr

--- api/app.js
+++ api/app.js
@@ -15,7 +15,7 @@ const server = require('http').createServer(app);
 
 const io = require('socket.io')(server, {
   cors: {
-    origin: process.env.CLIENT_URL,
+    origin: 'https://ekivokifront.vercel.app',
   },
 });
 
@@ -25,7 +25,7 @@ const errorMiddleware = require('./src/middlewares/error-middleware');
 const { PORT } = process.env; // получение переменных env
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL,
+  origin: [process.env.CLIENT_URL],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   credentials: true,
 };
